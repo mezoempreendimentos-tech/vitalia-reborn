dd129a5e677bbc694782efec4fedaeb0f5c3eb68
mezoempreendimentos-tech <mezoempreendimentos@gmail.com>
2025-08-25 12:29:23 -0300

    Update crafting.c
    Signed-off-by: mezoempreendimentos-tech <mezoempreendimentos@gmail.com>

--- src/crafting.c
+++ src/crafting.c
@@ -46,53 +46,64 @@ int list_length(struct recipe_data *list) {
 // Carrega as receitas do arquivo recipes.rcp
 void load_recipes(void) {
     FILE *fp;
-    char line[256], tag[128];
+    char line[256], tag[128], filename[256];
     struct recipe_data *current_recipe = NULL;
     int component_index = 0;
+    int i;
+    int total_recipes_loaded = 0;
 
-    if (!(fp = fopen(RECIPE_FILE, "r"))) {
-        log1("SYSERR: Não foi possível abrir o arquivo de receitas: %s", RECIPE_FILE);
-        return;
-    }
+    log1("...Carregando receitas de forma modular do diretório %s", RECIPE_PATH);
 
-    log1("...Carregando receitas de %s", RECIPE_FILE);
+    // Loop que varre todos os possíveis arquivos de perícia (1.rcp, 2.rcp, etc.)
+    for (i = 1; i <= MAX_CRAFT_SKILLS; i++) {
+        // Monta o nome do arquivo dinamicamente
+        snprintf(filename, sizeof(filename), "%s%d.rcp", RECIPE_PATH, i);
 
-    while (get_line(fp, line)) {
-        tag_argument(line, tag);
+        if (!(fp = fopen(filename, "r"))) {
+            // Se o arquivo não existe (ex: 3.rcp), simplesmente pula para o próximo.
+            continue;
+        }
 
-        if (str_cmp(tag, "Recipe") == 0) {
-            CREATE(current_recipe, struct recipe_data, 1);
-            component_index = 0;
-            
-            current_recipe->vnum = atoi(line);
-            current_recipe->station_vnum = -1;
-            current_recipe->enigma_name = NULL;
-            current_recipe->is_master_recipe = FALSE;
-            
-            current_recipe->next = recipe_list;
-            recipe_list = current_recipe;
+        // Se o arquivo existe, processa-o
+        while (get_line(fp, line)) {
+            tag_argument(line, tag);
 
-        } else if (current_recipe) {
-            if (str_cmp(tag, "Name") == 0) current_recipe->name = strdup(line);
-            else if (str_cmp(tag, "Enigma") == 0) current_recipe->enigma_name = strdup(line);
-            else if (str_cmp(tag, "ResultVnum") == 0) current_recipe->result_vnum = atoi(line);
-            else if (str_cmp(tag, "ResultQty") == 0) current_recipe->result_quantity = atoi(line);
-            else if (str_cmp(tag, "CrSkill") == 0) current_recipe->crskill_id = atoi(line); // CORRIGIDO
-            else if (str_cmp(tag, "Difficulty") == 0) current_recipe->difficulty = atoi(line);
-            else if (str_cmp(tag, "Station") == 0) current_recipe->station_vnum = atoi(line);
-            else if (str_cmp(tag, "Master") == 0) current_recipe->is_master_recipe = (atoi(line) == 1);
-            else if (str_cmp(tag, "Component") == 0) {
-                if (component_index < MAX_RECIPE_COMPONENTS) {
-                    sscanf(line, "%hd %d", 
-                        &current_recipe->components[component_index].vnum, 
-                        &current_recipe->components[component_index].quality_points);
-                    component_index++;
+            if (str_cmp(tag, "Recipe") == 0) {
+                CREATE(current_recipe, struct recipe_data, 1);
+                component_index = 0;
+                
+                current_recipe->vnum = atoi(line);
+                current_recipe->station_vnum = -1;
+                current_recipe->enigma_name = NULL;
+                current_recipe->is_master_recipe = FALSE;
+                
+                current_recipe->next = recipe_list;
+                recipe_list = current_recipe;
+                total_recipes_loaded++;
+
+            } else if (current_recipe) {
+                if (str_cmp(tag, "Name") == 0) current_recipe->name = strdup(line);
+                else if (str_cmp(tag, "Enigma") == 0) current_recipe->enigma_name = strdup(line);
+                else if (str_cmp(tag, "ResultVnum") == 0) current_recipe->result_vnum = atoi(line);
+                else if (str_cmp(tag, "ResultQty") == 0) current_recipe->result_quantity = atoi(line);
+                else if (str_cmp(tag, "CrSkill") == 0) current_recipe->crskill_id = atoi(line);
+                else if (str_cmp(tag, "Difficulty") == 0) current_recipe->difficulty = atoi(line);
+                else if (str_cmp(tag, "Station") == 0) current_recipe->station_vnum = atoi(line);
+                else if (str_cmp(tag, "Master") == 0) current_recipe->is_master_recipe = (atoi(line) == 1);
+                else if (str_cmp(tag, "Component") == 0) {
+                    if (component_index < MAX_RECIPE_COMPONENTS) {
+                        sscanf(line, "%hd %d", 
+                            &current_recipe->components[component_index].vnum, 
+                            &current_recipe->components[component_index].quality_points);
+                        component_index++;
+                    }
                 }
             }
         }
+        fclose(fp);
     }
-    fclose(fp);
-    log1("...%d receitas carregadas.", list_length(recipe_list));
+    
+    log1("...%d receitas carregadas de múltiplos arquivos.", total_recipes_loaded);
 }
 
 // Libera a memória das receitas
