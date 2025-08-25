bbb05fda267147212be0ecfb3f5af88b1b6dabb2
mezoempreendimentos-tech <mezoempreendimentos@gmail.com>
2025-08-25 13:56:49 -0300

    Create crskill.h
    Signed-off-by: mezoempreendimentos-tech <mezoempreendimentos@gmail.com>

---
+++ src/crskill.h
@@ -0,0 +1,19 @@
+#ifndef _CRSKILL_H_
+#define _CRSKILL_H_
+
+// Define o número máximo de perícias de crafting que o sistema suportará.
+#define MAX_CRAFT_SKILLS      50
+
+// Estrutura que define uma única perícia de crafting.
+struct craft_skill_data {
+    const char *name;
+};
+
+// Declaração do array global que conterá as definições das perícias.
+// O 'extern' diz que este array existe e será definido em outro arquivo (crskill.c).
+extern struct craft_skill_data craft_skills[];
+
+// Protótipo da função que irá preencher o array acima com as perícias.
+void assign_craft_skills(void);
+
+#endif
